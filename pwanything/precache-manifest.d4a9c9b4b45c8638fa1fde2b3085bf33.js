acdcb5c3dd3c3fccd2da1f541310b6a3061038b2
Felix <qzhao8731@gmail.com>
2021-02-23 10:20:04 -0800

    try pwanything

---
+++ pwanything/precache-manifest.d4a9c9b4b45c8638fa1fde2b3085bf33.js
@@ -0,0 +1,21 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae0b2110b774572c577bb8c713172a29",
+    "url": "/pwanything/index.html"
+  },
+  {
+    "url": "/pwanything/eeece9123acb4559517c80f8fba1f370.svg"
+  },
+  {
+    "revision": "09d6d9608257b414d1d8",
+    "url": "/pwanything/bundle.ea2e662a.css"
+  },
+  {
+    "revision": "09d6d9608257b414d1d8",
+    "url": "/pwanything/bundle.09d6d960.js"
+  },
+  {
+    "revision": "eedefc5f51907ad232a48404cd5ef1c4",
+    "url": "/pwanything/assets/favicon.png"
+  }
+];
