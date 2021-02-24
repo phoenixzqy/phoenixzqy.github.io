25e01cf48d81c003aca6720261ad97b72bf6c791
Felix <qzhao8731@gmail.com>
2021-02-23 16:16:08 -0800

    last try

--- pwanything/sw.js
+++ pwanything/sw.js
@@ -26,7 +26,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("index.html", {
+workbox.routing.registerNavigationRoute("/pwanything/app.html", {
   whitelist: [/^(?!\/__).*/],
   
 });
